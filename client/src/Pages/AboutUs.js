6a68896086303ac1f69f79bc88de1256231af623
Ayman Omer <ayman@macbook-pro.myfiosgateway.com>
2021-06-24 14:06:43 -0400

    add the img for home page

--- client/src/Pages/AboutUs.js
+++ client/src/Pages/AboutUs.js
@@ -1,9 +1,39 @@
 import React from 'react'
+import Katherine from './img/kathreen.png'
+import shan from './img/shan.png'
+import Ayman from './img/ayman.png'
 
 const AboutUs = () => {
+    const team =[
+        {
+            firstName: "Ayman",
+            lastName: "Omer",
+            teamTitle: "Full-Stack Web Developer",
+            teamImage: Ayman,
+            linkedin: "https://www.linkedin.com/in/ayman-omer-b2429b1ab/",
+          },
+          {
+            firstName: "Katherine",
+            lastName: "Fernandez",
+            teamTitle: "Product Designer",
+            teamImage: Katherine,
+            linkedin: "https://www.linkedin.com/in/katfernandez22/",
+          },
+          {
+            firstName: "Shan",
+            lastName: "Siddiqui",
+            teamTitle: "Front-End Web Developer",
+            teamImage:shan ,
+            linkedin: "https://www.linkedin.com/in/ssiddiqui007/",
+          },
+
+
+    ]
     return (
-        <div>
-           <h1>about </h1> 
+        <div className="team-page">
+
+
+           
         </div>
     )
 }
